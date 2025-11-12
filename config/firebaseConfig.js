d496aaf791c2054d4097eee0c6d7c2a02c968686
mantran138 <mantran1308@gmail.com>
2025-11-12 11:49:53 -0500

    Add files via upload

---
+++ config/firebaseConfig.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyASVOy90MT3iB2ZjLrPU7dI0lVE0Gm9-AE",
+    authDomain: "quizmaker-f4502.firebaseapp.com",
+    projectId: "quizmaker-f4502",
+    storageBucket: "quizmaker-f4502.firebasestorage.app",
+    messagingSenderId: "474282135859",
+    appId: "1:474282135859:web:9372753a0a99d01b7a6e46",
+    measurementId: "G-56K0EMVFMD"
+};
+
+export const initialAuthToken = null; // optional custom auth token
